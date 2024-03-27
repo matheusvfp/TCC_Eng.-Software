7712ad4cde31132f469ad9b4b78139c94fb7ab3c
Matheus Vinicius <matheusv83200@gmail.com>
2024-03-27 09:33:10 -0300

    Modified: Setup teardown Java Caculator

--- Java/JUnit/src/CalculadoraTest.java
+++ Java/JUnit/src/CalculadoraTest.java
@@ -1,11 +1,23 @@
 import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
+    private Calculadora calc;
+
+    @Before
+    public void setUp() {
+        calc = new Calculadora();
+    }
+
+    @After
+    public void tearDown() {
+        calc = null;
+    }
 
     @Test
     public void testSoma() {
-        Calculadora calc = new Calculadora();
         assertEquals(5, calc.soma(2, 3));
         assertEquals(-1, calc.soma(-2, 1));
         assertEquals(0, calc.soma(0, 0));
@@ -13,7 +25,6 @@ public class CalculadoraTest {
 
     @Test
     public void testSubtracao() {
-        Calculadora calc = new Calculadora();
         assertEquals(1, calc.subtracao(3, 2));
         assertEquals(-3, calc.subtracao(0, 3));
         assertEquals(0, calc.subtracao(10, 10));
@@ -21,7 +32,6 @@ public class CalculadoraTest {
 
     @Test
     public void testMultiplicacao() {
-        Calculadora calc = new Calculadora();
         assertEquals(6, calc.multiplicacao(2, 3), 0.001);
         assertEquals(0, calc.multiplicacao(0, 100), 0.001);
         assertEquals(-15, calc.multiplicacao(5, -3), 0.001);
@@ -29,7 +39,6 @@ public class CalculadoraTest {
 
     @Test
     public void testDivisao() {
-        Calculadora calc = new Calculadora();
         assertEquals(2, calc.divisao(6, 3), 0.001);
         assertEquals(0, calc.divisao(0, 100), 0.001);
         assertEquals(-2, calc.divisao(10, -5), 0.001);
@@ -37,7 +46,6 @@ public class CalculadoraTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testDivisaoPorZero() {
-        Calculadora calc = new Calculadora();
         calc.divisao(10, 0);
     }
 }
